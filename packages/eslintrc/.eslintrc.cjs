bb208820dfbb0c6d843cd63366a6bef83d387ea6
Quang Phan <vnphanquang@gmail.com>
2022-09-30 16:22:41 +0700

    chore(eslint): remove prettier from plugins array

--- packages/eslintrc/.eslintrc.cjs
+++ packages/eslintrc/.eslintrc.cjs
@@ -1,8 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const base = require('./base.cjs');
 
-module.exports = {
-  ...base,
-  extends: [...base.extends],
-  plugins: [...base.plugins, 'prettier'],
-};
+module.exports = base;
